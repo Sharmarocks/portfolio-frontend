370f5d40459bf56a4243158ab94d2b6b809159e3
Sharmarocks <rahulsharma199724@gmail.com>
2023-09-24 18:38:34 -0700

    updated main section

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import "./Main.scss";
+
+function Main() {
+  return (
+    <section>
+      <div className="main">
+        <div className="main__imgcontainer">
+          <img className="main__image" />
+        </div>
+        <div className="main__introconatiner">
+          <p className="main__intro">Hi,I'm Rahul a Web developer.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Main;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import Main from "../../components/Main/Main";
 
 function HomePage() {
   return (
     <section>
       <Header />
+      <Main />
     </section>
   );
 }
